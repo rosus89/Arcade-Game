8391b6b58f1a6891c555c01cb63a861af9a8272f
Stefan Rosu <rosus89@gmail.com>
2018-09-07 10:04:53 +0100

    Feature: added player and enemy class

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
-
+enemy = new Enemy(-100, 60, 100);
+let allEnemies = [];
+allEnemies.push(enemy);
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    return this.x += dt * this.speed;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -25,12 +26,42 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+    constructor(type) {
+        this.type = type;
+        this.x = 202;
+        this.y = 404;
+    }
+    handleInput(key) {
+        if (key === "up"){
+            this.y -= 83;
+        }
+        if (key === "down"){
+            this.y += 83;
+        }
+        if (key === "left"){
+            this.x -= 101;
+        }
+        if (key === "right"){
+            this.x += 101;
+        }
+    }
+    update(){
+
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.type), this.x, this.y)
+    }
+}
+
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+let selected = 'images/char-boy.png';
+let player = new Player(selected);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
